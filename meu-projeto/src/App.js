c24e5f9183892d68d349609285f7bd429a7d640a
Asael Honório <75455723+asaelhonorio@users.noreply.github.com>
2022-01-03 13:04:52 -0300

    .

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 
+// Função principal que faz tudo acontecer
 function App() {
+  var nome = 'Asael'
+  var nomeUpper = nome.toUpperCase()
+  // var btn = document.querySelector('button')
+
+  function soma(a, b) {
+    return a + b
+  }
+
   return (
     <div className="App">
       <h1>Olá mundo!</h1>
+      <h2>Alterando JSX</h2>
+      <h2>Olá {nomeUpper}</h2>
+      <h2>Soma: {soma(2, 3)}</h2>
+
     </div>
   );
 }
