eb2a08d1c47282344084ee1ca93ff7abb3c87f42
Asael Honório <75455723+asaelhonorio@users.noreply.github.com>
2022-01-18 09:35:30 -0300

    StateLift

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -9,52 +9,67 @@ import './App.css';
 // import Button from './components/evento/Button';
 // import Condicional from './components/Condicional';
 import RenderList from './components/RenderList';
+import SeuNome from './components/SeuNome';
+import { useState } from 'react'
+import Saudacao from './components/Saudacao';
 
 // Função principal que faz tudo acontecer
 function App() {
-/*  
-  var nome = 'Asael'
-  var nomeUpper = nome.toUpperCase()
+  /*  
+    var nome = 'Asael'
+    var nomeUpper = nome.toUpperCase()
+  
+  
+    function soma(a, b) {
+      return a + b
+    }
+  
+    return (
+      <div className="App">
+        <h1>Olá mundo!</h1>
+        <p>Alterando JSX</p>
+        <p>Olá {nomeUpper}</p>
+        <p>Soma: {soma(2, 3)}</p>
+        <HelloWorld />
+  
+        <PraticProps nome='Lucas' />
+  
+        <Pessoa nome='Pedro' idade='23' profissao='Programador' foto='https://via.placeholder.com/150' />
+        <List />
+  
+        <h3>======== TESTANDO EVENTOS=======</h3>
+        <Evento />
+  
+        <Form />
+        <h3>========== USE STATE ===========</h3>
+        <UseState />
+  
+        <h3>========== CONDITION ===========</h3>
+        <Condicional />
+      </div>
+  
+    );
+  */
+  // RENDERIZAÇÃO DE LISTA
+  /*    const meusItens = ['html5','css3','javaScript','react']
+      return(
+        <div className='App'>
+          <h1>Renderização de lista</h1>
+          <RenderList/>
+        </div>
+      )
+  */
 
-
-  function soma(a, b) {
-    return a + b
-  }
+  // STATE LIFT
+  const [nome, setNome] = useState()
 
   return (
-    <div className="App">
-      <h1>Olá mundo!</h1>
-      <p>Alterando JSX</p>
-      <p>Olá {nomeUpper}</p>
-      <p>Soma: {soma(2, 3)}</p>
-      <HelloWorld />
-
-      <PraticProps nome='Lucas' />
-
-      <Pessoa nome='Pedro' idade='23' profissao='Programador' foto='https://via.placeholder.com/150' />
-      <List />
-
-      <h3>======== TESTANDO EVENTOS=======</h3>
-      <Evento />
-
-      <Form />
-      <h3>========== USE STATE ===========</h3>
-      <UseState />
-
-      <h3>========== CONDITION ===========</h3>
-      <Condicional />
+    <div>
+      <h1>Utilizando StateLift</h1>
+      <SeuNome setNome={setNome} />
+      <Saudacao nome={nome} />
     </div>
-
-  );
-*/
-    // RENDERIZAÇÃO DE LISTA
-    const meusItens = ['html5','css3','javaScript','react']
-    return(
-      <div className='App'>
-        <h1>Renderização de lista</h1>
-        <RenderList/>
-      </div>
-    )
+  )
 }
 
 export default App;
---
+++ meu-projeto/src/components/Saudacao.js
@@ -0,0 +1,11 @@
+function Saudacao({nome}){
+    function gerarSaudacao(algumNome){
+        return `Olá ${algumNome}, tudo bem?`
+    }
+    return(
+        <>
+            {nome &&<p>{gerarSaudacao(nome)}</p>}
+        </>
+    )
+}
+export default Saudacao
